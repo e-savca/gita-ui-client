727435d0e617cf9a522cf52e9dea7b44cca632d7
EUGENIU SAVCA <eugeniu.savca@iongroup.com>
2025-08-01 13:31:36 +0300

    Finishing setup of electron + vite + react ts

--- gita-ui-client/src/main/index.ts
+++ gita-ui-client/src/main/index.ts
@@ -1,22 +1,34 @@
 import { app, BrowserWindow } from 'electron'
 import path from 'path'
 
-const createWindow = () => {
+const isDev = process.env.NODE_ENV === 'development'
+
+const createWindow = async () => {
   const win = new BrowserWindow({
     width: 800,
     height: 600,
     webPreferences: {
-      preload: path.join(__dirname, 'preload.js'), // optional
+      contextIsolation: true,
+      nodeIntegration: false,
     },
   })
 
+  if (isDev) {
+    const { default: installExtension, REACT_DEVELOPER_TOOLS } = await import(
+      'electron-devtools-installer'
+    )
+    const ext = await installExtension(REACT_DEVELOPER_TOOLS)
+    console.log(`React DevTools installed: ${ext.name}`)
+  }
+
   const url = process.env.VITE_DEV_SERVER_URL
   if (url) {
-    win.loadURL(url)
-    win.webContents.openDevTools()
+    await win.loadURL(url)
   } else {
-    win.loadFile(path.join(__dirname, '../renderer/index.html'))
+    await win.loadFile(path.join(__dirname, '../renderer/index.html'))
   }
+
+  if (isDev) win.webContents.reload()
 }
 
 app.whenReady().then(createWindow)
